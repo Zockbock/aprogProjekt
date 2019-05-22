9fe40f6c22cffc070d359111d8ee806e38d84138
Toffelpuffer <Toffelpuffer1@gmx.de>
2019-05-22 10:52:03 +0200

    added arrowkey controll

--- game/inputhandler.js
+++ game/inputhandler.js
@@ -5,23 +5,27 @@ class InputHandler {
         this.keyDown = context.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         this.keyLeft = context.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         this.keyRight = context.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        
+        this.keyAUp = context.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+        this.keyADown = context.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+        this.keyALeft = context.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        this.keyARight = context.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
 
-        console.log(this);
     }
     
     keydown(input){
         switch(input[0]){
             case 'u':
-                return this.keyUp.isDown;
+                return this.keyUp.isDown || this.keyAUp.isDown;
                 
             case 'd':
-                return this.keyDown.isDown;
+                return this.keyDown.isDown || this.keyADown.isDown;
 
             case 'l':
-                return this.keyLeft.isDown;
+                return this.keyLeft.isDown || this.keyALeft.isDown;
 
             case 'r':
-                return this.keyRight.isDown;
+                return this.keyRight.isDown || this.keyARight.isDown;
         }
 
         return false;
